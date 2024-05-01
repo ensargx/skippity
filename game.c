151a0aa594e6817f018e0f3cb41207df3bdebabf
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2024-05-01 23:10:32 +0300

    game started

---
+++ game.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef enum _Piece {
+    EMPTY = ' ',
+    BLUE = 'A',
+    GREEN = 'B',
+    YELLOW = 'C',
+    ORANGE = 'D',
+    RED = 'E',
+} Piece;
+
+typedef struct _Board {
+    int size;
+    Piece **cells;
+} Board;
+
+/* Initialize the board with random pieces */
+Board *initBoard(int N)
+{
+    int i, j, random;
+    Piece p;
+    Board *board;
+    if (N % 2 != 0) {
+        printf("Board size must be an even number\n");
+        return NULL;
+    }
+
+    board = (Board *)malloc(sizeof(Board));
+    board->size = N;
+    board->cells = (Piece **)malloc(N * sizeof(Piece *));
+    for (i = 0; i < N; i++)
+    {
+        board->cells[i] = (Piece *)malloc(N * sizeof(Piece));
+        for (j = 0; j < N; j++)
+        {
+            /* check if the cell is in the middle */
+            if ( (i == N/2-1 || i == N/2) && (j == N/2-1 || j == N/2) )
+            {
+                board->cells[i][j] = EMPTY;
+            }
+            else 
+            {
+                random = rand() % 5;
+                switch (random)
+                {
+                case 0:
+                    p = BLUE;
+                    break;
+                case 1:
+                    p = GREEN;
+                    break;
+                case 2:
+                    p = YELLOW;
+                    break;
+                case 3:
+                    p = ORANGE;
+                    break;
+                case 4:
+                    p = RED;
+                    break;
+                }
+                board->cells[i][j] = p;
+            }
+        }
+    }
+
+    return board;
+}
+
+/* Free the memory allocated for the board */
+void freeBoard(Board *board)
+{
+    int i;
+    for (i = 0; i < board->size; i++)
+    {
+        free(board->cells[i]);
+    }
+    free(board->cells);
+    free(board);
+}
+
+/* Print the board */
+void printBoard(Board *board)
+{
+    int i, j;
+    for (i = 0; i < board->size; i++)
+    {
+        for (j = 0; j < board->size; j++)
+        {
+            printf("%c ", board->cells[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int N;
+
+    srand(time(NULL));
+
+    printf("Enter the board size (n): ");
+    scanf("%d", &N);
+
+    Board *board = initBoard(N);
+    if (board == NULL)
+    {
+        return 1;
+    }
+
+    printBoard(board);
+
+    freeBoard(board);
+
+    return 0;
+}
+
